378ecc066ce45455b5be52a49f3d5ff06f6b958a
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-09-30 17:05:51 -0400

    Implemented trail files (saving)

--- src/main/java/net/inet_lab/starwars/SWApp.java
+++ src/main/java/net/inet_lab/starwars/SWApp.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 
 public class SWApp {
     public static void main(String[] args) throws InterruptedException, IOException {
-        TerminalEngine terminalEngine = new TerminalEngine();
+        TerminalEngine terminalEngine = new TerminalEngine().statusLine(1).trailFile("sw.trail");
         SWGame game = new SWGame(terminalEngine);
         terminalEngine.run(game);
+        terminalEngine.destroy();
     }
 }
--- src/main/java/net/inet_lab/starwars/TerminalEngine.java
+++ src/main/java/net/inet_lab/starwars/TerminalEngine.java
@@ -8,14 +8,25 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 public class TerminalEngine implements DisplayDriver, EventDriver {
     private final TextGraphics g;
-    private final Terminal term;
+    private Terminal term;
 
-    private final int status_lines = 1;
+    private int status_lines = 0;
+    private OutputStreamWriter trail = null;
+    private static final int TRAIL_VER = 1;
     private final double fps = 5;
     private final int X, Y;
+    boolean write_filed = false;
 
     @Override
     public int getWidth() {
@@ -24,7 +35,7 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
 
     @Override
     public int getHeight() {
-        return Y;
+        return Y - status_lines;
     }
 
     @Override
@@ -35,10 +46,11 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
     @Override
     public void msg(String text) {
         if (status_lines > 0) {
-            put(0, Y, " ".repeat(X));
-            put(0, Y, text);
+            put(0, Y-1, " ".repeat(X));
+            put(0, Y-1, text);
             flush();
         }
+        trail_write("# " + text);
     }
 
     @Override
@@ -52,15 +64,23 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
 
     @Override
     public void run(GameMove gameMove) throws InterruptedException {
-        gameMove.init(System.currentTimeMillis());
+        final long seed = System.currentTimeMillis();
+
+        trail_write("TRAIL " + TRAIL_VER + " " + X + " " + Y + " " + seed);
+
+        gameMove.init(seed);
+
+        int tick = 0;
 
         while(true) {
+            tick ++;
+
             KeyStroke key = null;
             Key gkey = null;
             try {
                 key = term.pollInput();
             } catch (IOException e) {
-                e.printStackTrace();
+                msg("poll failed " + e);
             }
             if (key != null) {
                 KeyType tkey = key.getKeyType();
@@ -69,6 +89,8 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
                     gkey = Key.ESC;
                 else if (tkey == KeyType.ArrowRight)
                     gkey = Key.RIGHT;
+                else if (tkey == KeyType.ArrowLeft)
+                    gkey = Key.LEFT;
                 else if (tchar != null) {
                     if (tchar == ' ')
                         gkey = Key.SPC;
@@ -76,18 +98,37 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
                         try {
                             gkey = Key.valueOf(tchar.toString().toUpperCase());
                         }
-                        catch (IllegalArgumentException _) {
+                        catch (IllegalArgumentException err) {
                             msg("Key <" + tchar + "> ignored");
                         }
                     }
                 }
             }
 
-            if(!gameMove.move(gkey))
+            if (gkey != null)
+                trail_write(tick + " K " + gkey);
+            if(!gameMove.move(gkey)) {
+                trail_write(tick + " E NORM");
+                destroy();
                 return;
+            }
             Thread.sleep((long) (1000 / fps));
         }
+    }
 
+    private void trail_write(String text) {
+        if (trail == null)
+            return;
+
+        try {
+            trail.write(text + "\n");
+            trail.flush();
+        } catch (IOException e) {
+            if (!write_filed) {
+                write_filed = true;
+                msg("Trail: " + e);
+            }
+        }
     }
 
     @Override
@@ -98,7 +139,47 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            term = null;
         }
+
+        if (trail != null) {
+            try {
+                trail.close();
+            } catch (IOException e) {
+                if (!write_filed) {
+                    write_filed = true;
+                    msg("Trail: " + e);
+                }
+            }
+            trail = null;
+        }
+    }
+
+    public TerminalEngine statusLine(int status_line) {
+        this.status_lines = status_line;
+        return this;
+    }
+
+    public TerminalEngine trailFile(String trail_file) {
+        if (trail_file != null) {
+            OutputStream out = null;
+            for(int v = 0; out == null && v < 1000; v ++) {
+                final String vfile = trail_file + ((v == 0) ? "" : ("." + v));
+                final Path p = Paths.get(vfile);
+                try {
+                    out = Files.newOutputStream(p, CREATE_NEW);
+                    trail = new OutputStreamWriter(out);
+                    msg("Writing trail to " + vfile);
+                } catch (FileAlreadyExistsException e) {
+                    trail = null;
+                } catch (IOException e) {
+                    trail = null;
+                    msg("Trail: " + e);
+                    break;
+                }
+            }
+        }
+        return this;
     }
 
     public TerminalEngine () throws IOException {
@@ -112,6 +193,6 @@ public class TerminalEngine implements DisplayDriver, EventDriver {
 
         final TerminalSize s = term.getTerminalSize();
         X = s.getColumns();
-        Y = s.getRows() - status_lines;
+        Y = s.getRows();
     }
 }
