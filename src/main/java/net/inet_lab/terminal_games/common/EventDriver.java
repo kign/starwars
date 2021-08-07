fc2ef9e70f5673a70c1cd56430b4fe91b6e06368
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-08-07 07:53:55 -0400

    Added `P` (for pause)

--- src/main/java/net/inet_lab/terminal_games/common/EventDriver.java
+++ src/main/java/net/inet_lab/terminal_games/common/EventDriver.java
@@ -2,7 +2,7 @@ package net.inet_lab.terminal_games.common;
 
 public interface EventDriver {
     enum Key {
-        Q, ESC, LEFT, RIGHT, SPC;
+        Q, P, ESC, LEFT, RIGHT, SPC;
     }
     void run(DisplayDriver.TerminalGame gameMove) throws InterruptedException;
 }
