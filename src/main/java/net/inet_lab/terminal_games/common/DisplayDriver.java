a23fb9bfe72854fd319af25dc4745876c06f0b18
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-08-07 07:53:39 -0400

    Added `log`, comments

--- src/main/java/net/inet_lab/terminal_games/common/DisplayDriver.java
+++ src/main/java/net/inet_lab/terminal_games/common/DisplayDriver.java
@@ -1,11 +1,23 @@
 package net.inet_lab.terminal_games.common;
 
 public interface DisplayDriver {
+    // screen size in characters
     int getWidth();
     int getHeight();
+
+    // Show text on screen
     void put(int x, int y, String text);
+
+    // Print user-visible message
     void msg(String text);
+
+    // Print debug message
+    void log(String text);
+
+    // flush updates
     void flush();
+
+    // cleanup
     void destroy();
 
     interface TerminalGame {
