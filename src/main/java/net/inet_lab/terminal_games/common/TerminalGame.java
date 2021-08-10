cacae0d003ce540bd15137b81eb47569c5984d81
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-08-09 20:15:39 -0400

    `move` now returns enum with 3 options

--- src/main/java/net/inet_lab/terminal_games/common/TerminalGame.java
+++ src/main/java/net/inet_lab/terminal_games/common/TerminalGame.java
@@ -1,6 +1,10 @@
 package net.inet_lab.terminal_games.common;
 
 public interface TerminalGame {
-    boolean move(EventDriver.Key key);
+    Status move(EventDriver.Key key);
     void init(DisplayDriver displayDriver, long seed);
+
+    enum Status {
+        CONT, WIN, LOSE;
+    }
 }
