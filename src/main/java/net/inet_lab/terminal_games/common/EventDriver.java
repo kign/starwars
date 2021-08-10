9323f8fe94f09bb3cb30dc46da1f639bf7152f84
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-08-09 20:15:10 -0400

    Added `IOException` to `run`

--- src/main/java/net/inet_lab/terminal_games/common/EventDriver.java
+++ src/main/java/net/inet_lab/terminal_games/common/EventDriver.java
@@ -1,8 +1,10 @@
 package net.inet_lab.terminal_games.common;
 
+import java.io.IOException;
+
 public interface EventDriver {
     enum Key {
         Q, P, ESC, LEFT, RIGHT, SPC;
     }
-    void run(TerminalGame gameMove) throws InterruptedException;
+    void run(TerminalGame gameMove) throws InterruptedException, IOException;
 }
